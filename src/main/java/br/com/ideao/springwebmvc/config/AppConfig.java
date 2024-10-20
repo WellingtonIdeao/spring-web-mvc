c89b593bd6f2f77eccbd8548901fe65557a0bb15
Wellington Ideão <wellington.ideao@gmail.com>
2024-10-19 23:52:39 -0300

    first commit

---
+++ src/main/java/br/com/ideao/springwebmvc/config/AppConfig.java
@@ -0,0 +1,9 @@
+package br.com.ideao.springwebmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.ideao.springwebmvc")
+public class AppConfig {
+}
