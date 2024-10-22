1ea80faa4744b7257ac39987149315eff421643f
Wellington Ideão <wellington.ideao@gmail.com>
2024-10-22 19:11:07 -0300

    feat: enable spring mvc web configuration

--- src/main/java/br/com/ideao/springwebmvc/config/AppConfig.java
+++ src/main/java/br/com/ideao/springwebmvc/config/AppConfig.java
@@ -2,8 +2,10 @@ package br.com.ideao.springwebmvc.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("br.com.ideao.springwebmvc")
+@EnableWebMvc
 public class AppConfig {
 }
